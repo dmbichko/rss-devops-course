a8a26e3d5d83fd0116a69c0ee2947a8175e1fb70
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-10-13 20:08:46 +0300

    terraform fmt

--- sg.tf
+++ sg.tf
@@ -31,22 +31,22 @@ resource "aws_security_group" "public_instances" {
   vpc_id      = aws_vpc.vpc-k8s.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = -1
