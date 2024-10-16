3f104aa8393b04e40a55874a7c4b73fab500e5b4
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-10-16 15:28:14 +0300

    added K3S_NODE_LABEL=true

--- ec2.tf
+++ ec2.tf
@@ -161,9 +161,10 @@ resource "aws_instance" "ec2-k8s-bastion" {
 
 resource "aws_instance" "k3s_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.ec2-instance-type
-  key_name      = aws_key_pair.EC2-instance_key.key_name
-  subnet_id     = aws_subnet.public_subnets[0].id
+  instance_type = "t3.medium"
+  #instance_type = var.ec2-instance-type
+  key_name  = aws_key_pair.EC2-instance_key.key_name
+  subnet_id = aws_subnet.public_subnets[0].id
 
   #install k3s server
   vpc_security_group_ids = [
