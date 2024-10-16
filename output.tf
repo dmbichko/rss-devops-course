06bc3dacab0d63a6c7a9e814e7efedefb56ea7f9
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-10-16 10:36:04 +0300

    added K3S_NODE_LABEL=true

--- output.tf
+++ output.tf
@@ -33,7 +33,6 @@ output "EC2_private_instance_details" {
 output "k3s-server" {
   value = [
     for instance in aws_instance.k3s_server : {
-      instance_id = instance.id
       public_ip   = instance.public_ip
       private_ip  = instance.private_ip
     }
