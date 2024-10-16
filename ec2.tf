d3250455893737103a0dcd9f8252e7a9e3393176
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-10-16 10:09:25 +0300

    added K3S_NODE_LABEL=true

--- ec2.tf
+++ ec2.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "ec2-k8s-private" {
                 echo "Waiting for K3s server to be ready..."
                 sleep 5
               done
-              curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_server.private_ip}:6443 K3S_TOKEN=${var.k3s_token} K3S_NODE_LABEL="node-role.kubernetes.io/worker=true" sh -s -
+              curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_server.private_ip}:6443 K3S_TOKEN=${var.k3s_token} sh -s - agent --kubelet-arg "node-labels=node-role.kubernetes.io/worker=worker"
               EOF
 
   tags = merge(
