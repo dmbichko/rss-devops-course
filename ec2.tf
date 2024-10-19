b59000cc3fb4a7b30b446fb9b7d63ea8c0df36b7
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-10-19 10:06:42 +0300

    removed public intancies and removed worker label

--- ec2.tf
+++ ec2.tf
@@ -29,14 +29,6 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-
-/*resource "time_sleep" "wait_for_k3s_server" {
-  depends_on = [aws_instance.k3s_server]
-
-  create_duration = "120s"
-}*/
-
-
 # Create a private key for aws instances
 resource "aws_key_pair" "EC2-instance_key" {
   key_name = "K8s-EC2-ssh-key"
@@ -58,18 +50,12 @@ resource "aws_instance" "nat" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.ec2-instance-type
   key_name      = aws_key_pair.EC2-instance_key.key_name
-  #subnet_id         = element(aws_subnet.public_subnets[*].id, count.index)
 
   network_interface {
     network_interface_id = aws_network_interface.nat_eni[count.index].id
     device_index         = 0
   }
 
-
-  # Security group configuration allowing SSH access
-  /*vpc_security_group_ids = [
-    aws_security_group.nat_sg.id
-  ]*/
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
@@ -88,8 +74,8 @@ resource "aws_instance" "nat" {
   )
 }
 
-
-resource "aws_instance" "ec2-k8s-public" {
+//Deleted these intances because of TAKS3
+/*resource "aws_instance" "ec2-k8s-public" {
   count         = length(aws_subnet.public_subnets[*].id)
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2-instance-type
@@ -137,7 +123,7 @@ resource "aws_instance" "ec2-k8s-private" {
     tomap({ "Name" = "${local.prefix}-ec2-k3s-agent-${count.index + 1}" })
   )
   depends_on = [aws_instance.k3s_server]
-}
+}*/
 
 resource "aws_instance" "ec2-k8s-bastion" {
   ami           = data.aws_ami.ubuntu.id
@@ -156,9 +142,9 @@ resource "aws_instance" "ec2-k8s-bastion" {
 }
 
 resource "aws_instance" "k3s_server" {
-  ami = data.aws_ami.ubuntu.id
-  #instance_type = "t3.medium"
-  instance_type        = var.ec2-instance-type
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.ec2-instance-type-k3s
+  #instance_type        = var.ec2-instance-type
   key_name             = aws_key_pair.EC2-instance_key.key_name
   subnet_id            = aws_subnet.public_subnets[0].id
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm_instance_profile.name
@@ -171,8 +157,6 @@ resource "aws_instance" "k3s_server" {
   user_data = <<-EOF
               #!/bin/bash
               curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --token ${var.k3s_token}
-              #--cluster-cidr 172.16.0.0/16 --service-cidr 172.17.0.0/16
-              #curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --token ${var.k3s_token} --cluster-cidr ${var.cidr}
               EOF
 
   tags = merge(
@@ -181,5 +165,35 @@ resource "aws_instance" "k3s_server" {
   )
 }
 
+resource "aws_instance" "ec2-k8s-private" {
+  count         = length(aws_subnet.private_subnets[1].id)
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.ec2-instance-type
+  key_name      = aws_key_pair.EC2-instance_key.key_name
 
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_instance_profile.name
+  subnet_id            = element(aws_subnet.private_subnets[*].id, count.index)
+
+  # Security group configuration allowing SSH access and icmp
+  vpc_security_group_ids = [
+    aws_security_group.allow_all_privata_sub.id
+  ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              until nc -z ${aws_instance.k3s_server.private_ip} 6443; do
+                echo "Waiting for K3s server to be ready..."
+                sleep 5
+              done
+              # Install K3s agent and register the worker node with the desired label
+              #curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_server.private_ip}:6443 K3S_TOKEN=${var.k3s_token} sh -s - agent --kubelet-arg="node-labels=node-role.kubernetes.io/worker=worker"
+              curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_server.private_ip}:6443 K3S_TOKEN=${var.k3s_token} sh -s - agent
+              EOF
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-ec2-k3s-agent-${count.index + 1}" })
+  )
+  depends_on = [aws_instance.k3s_server]
+}
 
--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-/*variable "ec2_public_key" {
-  description = "Name of the SSH key pair"
-  default     = "K8s-ec2-key"
-  type        = string
-}*/
-
 variable "k3s_token" {
   description = "K3S token"
   type        = string
@@ -27,6 +21,12 @@ variable "ec2-ssh-key" {
 variable "ec2-instance-type" {
   description = "EC2 instance type"
   default     = "t2.micro"
+  type        = string
+}
+
+variable "ec2-instance-type-k3s" {
+  description = "EC2 instance type"
+  default     = "t3.small"
   type        = string
 }
 
