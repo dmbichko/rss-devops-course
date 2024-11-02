ebd442a8c472b88fdf09be5261197ed6f29326f2
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-11-02 12:50:27 +0300

    management ec2

--- ec2-nginx-proxy.tf
+++ ec2-nginx-proxy.tf
@@ -1,18 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 data "template_file" "user_data" {
   template = file("${path.module}/nginx_proxy_userdata.sh")
 
